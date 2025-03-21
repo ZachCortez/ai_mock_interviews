cb8747a9c39233a3ed52c798633bf61847b02f05
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2025-03-21 15:22:02 -0400

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 
@@ -20,10 +21,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="dark">
-        <body className={`${monaSans.className} initialised pattern`}>
+        <body className={`${monaSans.className} antialiased pattern`}>
         {children}
 
-
+        <Toaster />
         </body>
         </html>
     );
